49eead5ccf6a03994e972eb050f3e771a6891e8a
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-24 12:27:32 -0700

    Set up page cards to render company logo, name, job title, and location - saved dummy data to avoid using api requests for time being

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -7,15 +7,22 @@ import {
   ActivityIndicator,
 } from "react-native";
 import { useRouter } from "expo-router";
+import useFetch from "../../../hooks/useFetch";
 
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
+import data from "./tempData.js";
+
 const Popularjobs = () => {
   const router = useRouter();
   const isLoading = false;
-  const error = false;
+  const error = null;
+  // const { data, isLoading, error } = useFetch("search", {
+  //   query: "React developer",
+  //   num_pages: 1,
+  // });
 
   return (
     <View style={styles.container}>
@@ -32,7 +39,7 @@ const Popularjobs = () => {
           <Text>Oops! Something went wrong!</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={data}
             renderItem={({ item }) => <PopularJobCard item={item} />}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
