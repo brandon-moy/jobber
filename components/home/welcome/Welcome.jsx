06a451f898cc9f4b6b62ba066b844a26cba540a6
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-24 10:58:44 -0700

    Create set up for search bar, added styles to welcome.style for padding and placeholdertextcolor for visibility

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View styles={styles.container}>
+        <Text style={styles.userName}>Hello world</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View styles={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+            placeholderTextColor={"lightgray"}
+          />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
--- components/home/welcome/welcome.style.js
+++ components/home/welcome/welcome.style.js
@@ -38,6 +38,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     paddingHorizontal: SIZES.medium,
+    paddingVertical: SIZES.small,
   },
   searchBtn: {
     width: 50,
