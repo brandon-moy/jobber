9458f4ea65f0ca72dfd3cc81c863e3f5f560e6db
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-24 15:15:42 -0700

    Set up About tab to display job about details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -46,9 +46,10 @@ const JobDetails = () => {
             points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
           />
         );
-
       case "About":
-
+        return (
+          <JobAbout info={data[0].job_description ?? "No data provided"} />
+        );
       case "Responsibilities":
 
       default:
